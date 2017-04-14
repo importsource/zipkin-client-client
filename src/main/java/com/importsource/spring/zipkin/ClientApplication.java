ef730700a2928e0625a5b07e0d0ae908ec2360b2
Charles <hezhuofan24@126.com>
2017-04-14 09:48:17 +0800

    Initial commit

---
+++ src/main/java/com/importsource/spring/zipkin/ClientApplication.java
@@ -0,0 +1,41 @@
+package com.importsource.spring.zipkin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.SpanAccessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@SpringBootApplication
+public class ClientApplication {
+
+  //  @Autowired
+  //  private SpanAccessor spanAccessor;
+
+
+
+
+	@Bean
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	@GetMapping("/hi")
+	public String hi(){
+   //     Span span = this.spanAccessor.getCurrentSpan();
+//restTemplate().
+     //   template.header(Span.TRACE_ID_NAME, Span.idToHex(span.getTraceId()));
+     //   setHeader(template, Span.SPAN_NAME_NAME, span.getName() );
+     //   setHeader(template, Span.SPAN_ID_NAME, Span.idToHex(span.getSpanId()));
+		return this.restTemplate().getForEntity("http://localhost:9081/hi",String.class).getBody();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+}
